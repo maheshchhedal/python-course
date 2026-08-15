36d5c96c70aed6c662052e928f6ea8fe58435c8e
maheshchheda <maheshchhedal0@gmail.com>
2026-08-16 00:04:06 +0545

    lambda function

---
+++ lambda/lam.py
@@ -0,0 +1,16 @@
+squar=lambda x: x*x
+print(squar(4))
+
+#conditon
+age=lambda x: 'you can drive ' if x>10 else 'go to hell'
+print(age(20))
+
+is_even=lambda x: 'even' if x %2==0 else 'odd'
+print(is_even(10))
+
+sums=lambda a,b: a+b
+print(sums(5,4))
+
+fullname=lambda first ,last: first + " "+last
+print(fullname('Ram','Bohara'))
+
